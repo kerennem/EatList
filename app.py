87071544d2b82c914696e390c4547672ccdf0183
yuliaSharabi <yudit77091@gmail.com>
2016-11-29 12:45:29 +0200

    1. Create tmp folder and move the app.py file. 2. Copy the app.py file to main folder EatList. 3. Split the app.py file in EatList to seperate files: views,run and app.

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 from flask import Flask
+
+
 app = Flask(__name__)
-
-@app.route("/hello")
-def index():
-    return "Hellow Flask "
-
-@app.route("/")
-def home:
-    return render_;
-if __name__ == '__main__':
-    app.run(debug=True)
+from app import views
---
+++ run.py
@@ -0,0 +1,3 @@
+#!flask/bin/python
+from EatList import app
+app.run(debug=True)
